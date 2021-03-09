563564c01388f1bfbad02489136d97dd65933e13
JoshLagoons <49170964+JoshLagoons@users.noreply.github.com>
2021-03-09 01:04:33 -0600

    Added a bit of text to the home page
    Adding text in the home page again to test making an edit in the github instead of visual code

--- src/home/home-page.js
+++ src/home/home-page.js
@@ -4,6 +4,7 @@ function HomePage() {
       <h1>Home</h1>
       <p>This is the home page.</p>
       <p>This is something new, enjoy!</p>
+      <p>Wrote this from my github, hoping to see if it theres change.</p>
     </main>
   );
 }
